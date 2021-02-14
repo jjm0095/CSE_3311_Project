a0b8d6dd75d6d2e0a271b64a82767086793bde85
Antony Nguyen <antony.nguyen@mavs.uta.edu>
2021-02-13 21:26:11 -0600

    updated timer

--- WebApplication3/ClientApp/src/app/app.module.ts
+++ WebApplication3/ClientApp/src/app/app.module.ts
@@ -9,7 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { LoginComponent } from './login/login.component'
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
@@ -29,7 +31,9 @@ import { LoginComponent } from './login/login.component'
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
       { path: 'login', component: LoginComponent },
-    ])
+    ]),
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- WebApplication3/ClientApp/src/app/home/home.component.ts
+++ WebApplication3/ClientApp/src/app/home/home.component.ts
@@ -1,16 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { PortalModule } from '@angular/cdk/portal';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit{
+
+
+export class HomeComponent implements OnInit {
+secondsToString(value: number): string {
+  var date = new Date(0);
+  date.setSeconds(value);
+  var timeString = date.toISOString().substr(14, 5);
+  return timeString;
+}
+
+  intervals = {
+    pomodoro: 1500,
+    shortBreak: 300,
+    longBreak: 900,
+  };
+
+  pomodoroTimer = {
+    timeSeconds: this.intervals.pomodoro,
+    timeString: this.secondsToString(this.intervals.pomodoro),
+    buttonText: "Start",
+    timerId: null,
+    paused: true,
+  };
+
+  shortBreakTimer = {
+    timeSeconds: this.intervals.shortBreak,
+    timeString: this.secondsToString(this.intervals.shortBreak),
+    buttonText: "Start",
+    timerId: null,
+    paused: true,
+  };
+
+  longBreakTimer = {
+    timeSeconds: this.intervals.longBreak,
+    timeString: this.secondsToString(this.intervals.longBreak),
+    buttonText: "Start",
+    timerId: null,
+    paused: true,
+  }
+
   timer: number = 90;
   pause: boolean = false;
   constructor() { }
+
   ngOnInit() {
 
   }
+
   countdown() {
     
       let intervalID = setInterval(() => {
@@ -32,4 +74,132 @@ export class HomeComponent implements OnInit{
       this.pause = false;
     }
   }
+
+  getButtonText(value: boolean): string {
+    return value ? "Start" : "Pause";
+  }
+
+  onPomodoroButton() {
+    // Reset the timer if time has reached 0
+    if (!this.pomodoroTimer.timeSeconds) {
+      if (this.pomodoroTimer.timerId) {
+        clearInterval(this.pomodoroTimer.timerId);
+        this.pomodoroTimer.timerId = null;
+      }
+      this.pomodoroTimer.timeSeconds = this.intervals.pomodoro;
+      this.loopPomodoro();
+    }
+    else {
+      // Start timer (first time)
+      if (!this.pomodoroTimer.timerId) {
+        this.pomodoroTimer.paused = false;
+        this.pomodoroTimer.buttonText = this.getButtonText(this.pomodoroTimer.paused);
+        this.loopPomodoro();
+      }
+      else {
+        // Pause/Resume the timer
+        this.pomodoroTimer.paused = !this.pomodoroTimer.paused;
+        this.pomodoroTimer.buttonText = this.getButtonText(this.pomodoroTimer.paused);
+      }
+    }
+  }
+
+  loopPomodoro() {
+    this.pomodoroTimer.timerId = setInterval(() => {
+      if (0 >= this.pomodoroTimer.timeSeconds) {
+        if (this.pomodoroTimer.timerId) {
+          clearInterval(this.pomodoroTimer.timerId);
+          this.pomodoroTimer.timerId = null;
+        }
+        return;
+      }
+      if (!this.pomodoroTimer.paused) {
+        this.pomodoroTimer.timeSeconds--;
+        this.pomodoroTimer.timeString = this.secondsToString(this.pomodoroTimer.timeSeconds);
+      }
+    }, 1000);
+  }
+
+  onShortBreakButton() {
+    // Reset the timer if time has reached 0
+    if (!this.shortBreakTimer.timeSeconds) {
+      if (this.shortBreakTimer.timerId) {
+        clearInterval(this.shortBreakTimer.timerId);
+        this.shortBreakTimer.timerId = null;
+      }
+      this.shortBreakTimer.timeSeconds = this.intervals.shortBreak;
+      this.loopShortBreak();
+    }
+    else {
+      // Start timer (first time)
+      if (!this.shortBreakTimer.timerId) {
+        this.shortBreakTimer.paused = false;
+        this.shortBreakTimer.buttonText = this.getButtonText(this.shortBreakTimer.paused);
+        this.loopShortBreak();
+      }
+      else {
+        // Pause/Resume the timer
+        this.shortBreakTimer.paused = !this.shortBreakTimer.paused;
+        this.shortBreakTimer.buttonText = this.getButtonText(this.shortBreakTimer.paused);
+      }
+    }
+  }
+
+  loopShortBreak() {
+    this.shortBreakTimer.timerId = setInterval(() => {
+      if (0 >= this.shortBreakTimer.timeSeconds) {
+        if (this.shortBreakTimer.timerId) {
+          clearInterval(this.shortBreakTimer.timerId);
+          this.shortBreakTimer.timerId = null;
+        }
+        return;
+      }
+      if (!this.shortBreakTimer.paused) {
+        this.shortBreakTimer.timeSeconds--;
+        this.shortBreakTimer.timeString = this.secondsToString(this.shortBreakTimer.timeSeconds);
+      }
+    }, 1000);
+  }
+
+  onLongBreakButton() {
+    // Reset the timer if time has reached 0
+    if (!this.longBreakTimer.timeSeconds) {
+      if (this.longBreakTimer.timerId) {
+        clearInterval(this.longBreakTimer.timerId);
+        this.longBreakTimer.timerId = null;
+      }
+      this.longBreakTimer.timeSeconds = this.intervals.longBreak;
+      this.loopLongBreak();
+    }
+    else {
+      // Start timer (first time)
+      if (!this.longBreakTimer.timerId) {
+        this.longBreakTimer.paused = false;
+        this.longBreakTimer.buttonText = this.getButtonText(this.longBreakTimer.paused);
+        this.loopLongBreak();
+      }
+      else {
+        // Pause/Resume the timer
+        this.longBreakTimer.paused = !this.longBreakTimer.paused;
+        this.longBreakTimer.buttonText = this.getButtonText(this.longBreakTimer.paused);
+      }
+    }
+  }
+
+  loopLongBreak() {
+    this.longBreakTimer.timerId = setInterval(() => {
+      if (0 >= this.longBreakTimer.timeSeconds) {
+        if (this.longBreakTimer.timerId) {
+          clearInterval(this.longBreakTimer.timerId);
+          this.longBreakTimer.timerId = null;
+        }
+        return;
+      }
+      if (!this.longBreakTimer.paused) {
+        this.longBreakTimer.timeSeconds--;
+        this.longBreakTimer.timeString = this.secondsToString(this.longBreakTimer.timeSeconds);
+      }
+    }, 1000);
+  }
+
 }
