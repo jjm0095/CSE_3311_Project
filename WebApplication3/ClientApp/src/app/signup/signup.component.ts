65376422adb8439d922f7e3e48d45ef2100084d1
jjm0095 <jonathan.medina3@mavs.uta.edu>
2021-04-05 22:42:38 -0500

    added code for sign up screen

--- WebApplication3/ClientApp/src/app/signup/signup.component.ts
+++ WebApplication3/ClientApp/src/app/signup/signup.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from './../models/User';
 import { DataService } from './../data.service';
+import { MatSnackBar } from '@angular/material';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
 })
 export class SignupComponent implements OnInit {
+  constructor(private data: DataService, private _dialogBar: MatSnackBar) { }
   ngOnInit(): void {
 
   }
+
+  newU: User;
 
   inputUName: string = "";
   inputEmail: string = "";
@@ -26,6 +30,40 @@ export class SignupComponent implements OnInit {
   newU: User;
   */
   onSignUp() {
-    
+    /*
+    if (!(this.inputEmail == '') && !(this.inputPassword == '') && (this.inputPassword == this.inputPasswordConfrim) && !(this.inputUName == '')) {
+      this.newU.email = this.inputEmail;
+      this.newU.userName = this.inputUName;
+      this.newU.password_ = this.inputPassword;
+
+      this.openAccountSuccessBar()
+    } else if (this.inputPassword != this.inputPasswordConfrim) {
+      this.openPasswordMisMatchBar()
+    } else {
+      this.openAccountFailBar()
+    }
+    */
   }
+  /*
+  openAccountSuccessBar() {
+    let undoBarRef = this._dialogBar.open("Account \""+ this.inputUName +"\" was created", "X", { duration: 10000, });
+    undoBarRef.onAction().subscribe(() => {
+      this._dialogBar.dismiss();
+    });
+  }
+
+  openPasswordMisMatchBar() {
+    let undoBarRef = this._dialogBar.open("Please confrim your password", "X", { duration: 10000, });
+    undoBarRef.onAction().subscribe(() => {
+      this._dialogBar.dismiss();
+    });
+  }
+
+  openAccountFailBar() {
+    let undoBarRef = this._dialogBar.open("One of the fields was invalid", "X", { duration: 10000, });
+    undoBarRef.onAction().subscribe(() => {
+      this._dialogBar.dismiss();
+    });
+  }
+  */
 }
